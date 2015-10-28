0ddaa7d35bec8197d8528ff47faa22e1547725eb
Antoine Lenoir <antoine.alenoir@gmail.com>
2015-10-28 11:03:38 +0100

    add volume controls

--- android/src/main/java/com/tricy/RNAudioPlayer/RNAudioPlayerModule.java
+++ android/src/main/java/com/tricy/RNAudioPlayer/RNAudioPlayerModule.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.lang.Exception;
 
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -20,12 +21,14 @@ public class RNAudioPlayerModule extends ReactContextBaseJavaModule {
   ReactApplicationContext reactContext;
   MediaPlayer mp;
   Map<String,MediaPlayer> sounds;
+  Map<String,Float> soundVolumes;
   public static final String TAG = "-- SOUNDPAD --";
 
   public RNAudioPlayerModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
     sounds = new HashMap<>();
+    soundVolumes = new HashMap<>();
   }
 
   @Override
@@ -40,28 +43,80 @@ public class RNAudioPlayerModule extends ReactContextBaseJavaModule {
     try {
       mp = MediaPlayer.create(this.reactContext, resID);
       sounds.put(audio, mp);
+      soundVolumes.put(audio, (float) 1.0);
     } catch(Exception e) {
 
     }
   }
 
-  @ReactMethod
-  public void play(final String audio) {
+  public void prepareAndPlay(String audio, boolean loop) {
+    this.prepare(audio);
     mp = sounds.get(audio);
     if (mp != null) {
+      this.handlePlay(audio, loop);
+    }
+  }
+
+  @ReactMethod
+  public void play(String audio) {
+    this.handlePlay(audio, false);
+  }
+
+  @ReactMethod
+  public void playWithLoop(String audio) {
+    this.handlePlay(audio, true);
+  }
+
+  public void handlePlay(final String audio, boolean loop) {
+    mp = sounds.get(audio);
+    if (mp == null) {
+      this.prepareAndPlay(audio, loop);
+    } else {
       try {
         mp = sounds.get(audio);
-
+        mp.setLooping(loop);
         mp.start();
-        mp.setOnCompletionListener(new OnCompletionListener() {
-          @Override
-          public void onCompletion(MediaPlayer mp) {
-            RNAudioPlayerModule.this.stop(audio);
-          }
-        });
+        if (!loop) {
+          mp.setOnCompletionListener(new OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+              RNAudioPlayerModule.this.stop(audio);
+            }
+          });
+        }
       } catch (Exception e) {
 
       }
+    }
+  }
+
+  @ReactMethod
+  public void increaseVolume(String audio) {
+    mp = sounds.get(audio);
+    if (mp != null) try {
+      float soundVolume = soundVolumes.get(audio);
+      if (soundVolume < 1.0) {
+        soundVolume = soundVolume + 0.1f;
+      }
+      soundVolumes.put(audio, soundVolume);
+      mp.setVolume(soundVolume, soundVolume);
+    } catch (Exception e) {
+
+    }
+  }
+
+  @ReactMethod
+  public void decreaseVolume(String audio) {
+    mp = sounds.get(audio);
+    if (mp != null) try {
+      float soundVolume = soundVolumes.get(audio);
+      if (soundVolume > 0.0) {
+        soundVolume = soundVolume - 0.1f;
+      }
+      soundVolumes.put(audio, soundVolume);
+      mp.setVolume(soundVolume, soundVolume);
+    } catch (Exception e) {
+
     }
   }
 
